2da884c6cffda4359f25ec7a3d5833462808c565
paukasha <odinpozitivchik@mail.ru>
2022-06-13 11:10:54 +1000

    commit2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {store} from './redux';
 
 
 render(<Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App/>
         </BrowserRouter>
   </Provider>,  document.getElementById('root')
